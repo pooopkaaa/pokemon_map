2dac98bf2d00896c84e09d54d4de09cdbde6810d
pooopkaaa <60960322+pooopkaaa@users.noreply.github.com>
2021-04-23 12:12:05 +0300

    Add PokemonEntity model to site

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -4,6 +4,7 @@ import json
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from pokemon_entities.models import Pokemon
+from pokemon_entities.models import PokemonEntity
 
 
 MOSCOW_CENTER = [55.751244, 37.618423]
@@ -28,17 +29,16 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 
 def show_all_pokemons(request):
-    with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
-        pokemons = json.load(database)['pokemons']
-
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon in pokemons:
-        for pokemon_entity in pokemon['entities']:
-            add_pokemon(
-                folium_map, pokemon_entity['lat'],
-                pokemon_entity['lon'],
-                pokemon['img_url']
-            )
+    pokemon_entities = PokemonEntity.objects.all()
+    for pokemon_entity in pokemon_entities:
+        pokemon_img_url = request.build_absolute_uri(pokemon_entity.pokemon.image.url)
+        add_pokemon(
+            folium_map, 
+            pokemon_entity.lat,
+            pokemon_entity.lon,
+            pokemon_img_url
+        )
 
     pokemons = Pokemon.objects.all()
     pokemons_on_page = []
