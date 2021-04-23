07889fc68c52d4920034cef709133d193115ef77
pooopkaaa <60960322+pooopkaaa@users.noreply.github.com>
2021-04-23 15:03:57 +0300

    Add models to pokemon.html

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -61,24 +61,23 @@ def show_all_pokemons(request):
 
 
 def show_pokemon(request, pokemon_id):
-    with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
-        pokemons = json.load(database)['pokemons']
-
-    for pokemon in pokemons:
-        if pokemon['pokemon_id'] == int(pokemon_id):
-            requested_pokemon = pokemon
-            break
-    else:
-        return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
+    pokemon = Pokemon.objects.get(id=pokemon_id)
+    pokemon_img_url = request.build_absolute_uri(pokemon.image.url)
+    pokemon_on_page = {
+        'title_ru': pokemon.title,
+        'img_url': pokemon_img_url
+    }
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon_entity in requested_pokemon['entities']:
+    pokemon_entities = PokemonEntity.objects.filter(pokemon=pokemon)
+    for pokemon_entity in pokemon_entities:
         add_pokemon(
-            folium_map, pokemon_entity['lat'],
-            pokemon_entity['lon'],
-            pokemon['img_url']
+            folium_map, 
+            pokemon_entity.lat,
+            pokemon_entity.lon,
+            pokemon_img_url
         )
 
     return render(request, 'pokemon.html', context={
-        'map': folium_map._repr_html_(), 'pokemon': pokemon
+        'map': folium_map._repr_html_(), 'pokemon': pokemon_on_page
     })
